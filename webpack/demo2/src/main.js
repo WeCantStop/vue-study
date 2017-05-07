6310221f1410f9e6c5efa4fcbb4828f74c5cc17e
Will <pan123033@qq.com>
2017-05-08 00:33:12 +0800

    webpack配置loader demo

--- webpack/demo2/src/main.js
+++ webpack/demo2/src/main.js
@@ -1,5 +1,8 @@
 /**
  * Created by Will on 2017.5.5.
  */
+require("./css/index.css");
+require("./css/index.scss");
+
 var add = require("./cacl.js");
-console.log(add(44,11))
+console.log(add(44, 11))
--- webpack/demo2/webpack.config.js
+++ webpack/demo2/webpack.config.js
@@ -1,10 +1,24 @@
 //var webpack = require("webpack");
+var path = require("path");
+
 module.exports = {
     //1.0 定义打包的入口文件路径 entry
     entry: "./src/main.js",
     //2.0 打包完成之后生成文件的路径和文件名
     output: {
-        path: "./dist",
+        path: path.resolve(__dirname, 'dist'),
         filename: "build.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.scss$/,
+                loader: "style-loader!css-loader!sass-loader"
+            }
+        ]
     }
 }
