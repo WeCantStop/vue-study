bdcd1588e6acb3bc0291fa3a70e28e18dca91297
Will <pan123033@qq.com>
2017-05-09 22:59:51 +0800

    打包img,file-loader,url-loader,demo

--- webpack/demo2/src/main.js
+++ webpack/demo2/src/main.js
@@ -3,6 +3,7 @@
  */
 require("./css/index.css");
 require("./css/index.scss");
+require("./mui/css/icons-extra.css");
 
 var add = require("./cacl.js");
 console.log(add(44, 11))
--- webpack/demo2/webpack.config.js
+++ webpack/demo2/webpack.config.js
@@ -12,12 +12,21 @@ module.exports = {
     module: {
         loaders: [
             {
+                //打包css
                 test: /\.css$/,
                 loader: "style-loader!css-loader"
             },
             {
+                //打包sass
                 test: /\.scss$/,
                 loader: "style-loader!css-loader!sass-loader"
+            },
+            {
+                //打包图片url形式的
+                //转换为base_64的字符串存储到build.js中去,太大会导致效率低下
+                test: /\.(jpg|png|ttf|gif)$/,
+                //限制图片的大小，小于40k就转化成base_64字符串存储到build.js中去
+                loader: "url-loader?limit=50000"
             }
         ]
     }
