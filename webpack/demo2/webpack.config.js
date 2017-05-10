83fd0d3f48c360b5510b3909473ce221d1e210f4
Will <pan123033@qq.com>
2017-05-11 00:32:12 +0800

    webpack-dev-server --hot --inline --open

--- webpack/demo2/webpack.config.js
+++ webpack/demo2/webpack.config.js
@@ -1,11 +1,14 @@
 //var webpack = require("webpack");
 var path = require("path");
+//获取到插件对象
+var htmlWp = require("html-webpack-plugin")
 
 module.exports = {
     //1.0 定义打包的入口文件路径 entry
     entry: "./src/main.js",
     //2.0 打包完成之后生成文件的路径和文件名
     output: {
+        publicPath:'/',
         path: path.resolve(__dirname, 'dist'),
         filename: "build.js"
     },
@@ -37,5 +40,13 @@ module.exports = {
                 loader: "url-loader?limit=50000"
             }
         ]
-    }
+    },
+    plugins: [
+        new htmlWp({
+            title: "首页",
+            filename: "index.html",
+            template: "index.html"
+        })
+    ]
+
 }
